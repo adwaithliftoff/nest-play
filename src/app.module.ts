47c7fd7d0a4a31897b460b2ea706f3262d8e9942
adwaithliftoff <269126028+adwaithliftoff@users.noreply.github.com>
2026-03-23 09:19:38 +0530

    Add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './user.service';
 import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { PostsService } from './post.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, UsersService, PrismaService, PostsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('');
+  }
+}
